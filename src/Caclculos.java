0f03832bf5751b96c305171b6b7617ff36eff776
Elizenok <liza_cat@mail.ru>
2023-02-16 23:29:20 +0300

    Add public static String calc(String input)

--- src/Caclculos.java
+++ src/Caclculos.java
@@ -1,48 +1,62 @@
 import java.io.IOException;
+import java.util.*;
 import java.util.Scanner;
 
 public class Caclculos {
-    public static void main(String[] args) throws IOException  {
-        int value1 = 0;
-        int value2 = 0;
-        String operation = null;
-        System.out.print("Введите выражение (через пробелы): ");
-        Calculator calc = new Calculator();
-        calc.calculate(value1,value2, operation);
-    }
-}
 
-class Calculator {
-    Scanner scanner = new Scanner(System.in);
-
-    void calculate(int value1, int value2, String operation) throws IOException {
-
-        value1 = scanner.nextInt();
-        operation = scanner.next();
-        value2 = scanner.nextInt();
-        if (value1 < 1 || value1 > 10) {
-            throw new IOException("number error 1");
-        }
-        if (value2 < 1 || value2 > 10) {
-            throw new IOException("number error 2");
-
-        }
-        if (operation.equals("+")) {
-            System.out.println(value1 + value2);
-        }
-        else if (operation.equals("-")) {
-            System.out.println(value1 - value2);
-        }
-        else if (operation.equals("*")) {
-            System.out.println(value1 * value2);
-        }
-        else if (operation.equals("/")) {
-            System.out.println(value1 / value2);
+    public static String[] calc(String inputString) throws IOException {
+        String[] calcInputString = inputString.split(" ");
+        if (calcInputString.length != 3) {
+            throw new IOException("Неверный формат ввода данных. Введите выражение, разделяя каждый символ _пробелом");
         } else {
-            throw new IOException(" operator error");
-
+            return calcInputString;
         }
     }
 
-}
+    public static void main(String[] args) throws IOException {
+        Scanner inputStringValue = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String inputString = inputStringValue.nextLine();
+        if (!inputString.isEmpty()) {
+            String[] calcInputString;
+            calcInputString = calc(inputString);
+            String operation = calcInputString[1];
+            Number values;
+            int value1 = 0;
+            int value2 = 0;
+
+            try {
+                value1 = Integer.parseInt(calcInputString[0]);
+                value2 = Integer.parseInt(calcInputString[2]);
+            } catch (NumberFormatException e) {
+                System.out.println("Неправильный формат числа");
+            }
+            if (value1 < 1 || value1 > 10) {
+                throw new IOException("number error 1");
+            }
+            if (value2 < 1 || value2 > 10) {
+                throw new IOException("number error 2");
+
+            }
+            if (operation.equals("+")) {
+                System.out.println(value1 + value2);
+            }
+            else if (operation.equals("-")) {
+                System.out.println(value1 - value2);
+            }
+            else if (operation.equals("*")) {
+                System.out.println(value1 * value2);
+            }
+            else if (operation.equals("/")) {
+                System.out.println(value1 / value2);
+            } else {
+                throw new IOException(" operator error");
+
+            }
+
+            }
+        }
+    }
+
+
 

1d99d1968f31d9e7f02c6a8a55ea73864ab30ddd
Elizenok <liza_cat@mail.ru>
2023-03-01 01:58:10 +0300

    Add public static String calc(String input)

--- src/Caclculos.java
+++ src/Caclculos.java
@@ -1,62 +1,63 @@
 import java.io.IOException;
-import java.util.*;
 import java.util.Scanner;
 
 public class Caclculos {
 
     public static String[] calc(String inputString) throws IOException {
         String[] calcInputString = inputString.split(" ");
+        if (inputString.isEmpty()) {
+            throw new IOException("Выражение не введено");
+        }
         if (calcInputString.length != 3) {
             throw new IOException("Неверный формат ввода данных. Введите выражение, разделяя каждый символ _пробелом");
-        } else {
+        }
+        else {
             return calcInputString;
         }
     }
-
     public static void main(String[] args) throws IOException {
         Scanner inputStringValue = new Scanner(System.in);
         System.out.print("Введите выражение: ");
         String inputString = inputStringValue.nextLine();
-        if (!inputString.isEmpty()) {
-            String[] calcInputString;
-            calcInputString = calc(inputString);
-            String operation = calcInputString[1];
-            Number values;
-            int value1 = 0;
-            int value2 = 0;
 
-            try {
-                value1 = Integer.parseInt(calcInputString[0]);
-                value2 = Integer.parseInt(calcInputString[2]);
-            } catch (NumberFormatException e) {
-                System.out.println("Неправильный формат числа");
-            }
-            if (value1 < 1 || value1 > 10) {
-                throw new IOException("number error 1");
-            }
-            if (value2 < 1 || value2 > 10) {
-                throw new IOException("number error 2");
+        String[] calcInputString;
+        calcInputString = calc(inputString);
+        String operation = calcInputString[1];
+        int value1 = 0;
+        int value2 = 0;
 
-            }
-            if (operation.equals("+")) {
-                System.out.println(value1 + value2);
-            }
-            else if (operation.equals("-")) {
-                System.out.println(value1 - value2);
-            }
-            else if (operation.equals("*")) {
-                System.out.println(value1 * value2);
-            }
-            else if (operation.equals("/")) {
-                System.out.println(value1 / value2);
-            } else {
-                throw new IOException(" operator error");
-
-            }
-
-            }
+        try {
+            value1 = Integer.parseInt(calcInputString[0]);
+            value2 = Integer.parseInt(calcInputString[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("Неправильный формат числа");
         }
+        if (value1 < 1 || value1 > 10) {
+            throw new IOException("number error 1");
+        }
+        if (value2 < 1 || value2 > 10) {
+            throw new IOException("number error 2");
+
+        }
+        if (operation.equals("+")) {
+            System.out.println(value1 + value2);
+        } else if (operation.equals("-")) {
+            System.out.println(value1 - value2);
+        } else if (operation.equals("*")) {
+            System.out.println(value1 * value2);
+        } else if (operation.equals("/")) {
+            System.out.println(value1 / value2);
+        } else {
+            throw new IOException(" operator error");
+
+        }
+
     }
+
+}
+
+
+
 
 
 
